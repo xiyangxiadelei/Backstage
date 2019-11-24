a40d5d6cd9980680d3b918b53a709ecb51b02add
Leisure <466851998@@qq.com>
2019-11-24 19:21:13 +0800

    第一次修改

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '教育系统',
 
   /**
    * @type {boolean} true | false
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">当前登入用户: {{ name }}</div>
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
+  name: '首页',
   computed: {
     ...mapGetters([
       'name'
